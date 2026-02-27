757edeffdcd14954fd9ee0e416c9f524ab2d3c7e
jasmix555 <jasmix555@gmail.com>
2026-02-27 19:34:26 +0900

    chore: learn array, mapping/foreach

---
+++ php_lesson/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <?php
+    $books = [
+        [
+            'name' => "book 1",
+            "author" => "author 1",
+            "purchaseUrl" => "https://example.com"
+        ],
+
+        [
+            'name' => "book 2",
+            "author" => "author 2",
+            "purchaseUrl" => "https://example.com"
+        ],
+
+        [
+            'name' => "book 3",
+            "author" => "author 3",
+            "purchaseUrl" => "https://example.com"
+        ]
+    ];
+    ?>
+
+
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li>
+                <?= $book["name"]; ?>
+                <br>
+                by <?= $book["author"] ?>
+                <br>
+                <a href="<?= $book['purchaseUrl'] ?>">Purchase Here</a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+
+</html>
