c72a864a99142ecc49e1df27f974eafc4f452d97
jasmix555 <jasmix555@gmail.com>
2026-03-05 22:59:53 +0900

    chore: start chapter 2

--- php_lesson/index.php
+++ php_lesson/index.php
@@ -1,16 +1,4 @@
-
 <?php
-
-$business = [
-  "name" => "Laracasts",
-  "cost" => 15,
-  "categories" => [
-    "Testing",
-    "PHP",
-    "JavaScript"
-  ]
-];
-
 
 
 require "index.view.php";
---
+++ php_lesson/tutorials/chapter-01/part-02/index.php
@@ -0,0 +1,16 @@
+
+<?php
+
+$business = [
+  "name" => "Laracasts",
+  "cost" => 15,
+  "categories" => [
+    "Testing",
+    "PHP",
+    "JavaScript"
+  ]
+];
+
+
+
+require "index.view.php";
