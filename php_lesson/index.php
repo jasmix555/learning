48ce34504e87ac0fc95616a9ef9766accbf6c2fe
jasmix555 <jasmix555@gmail.com>
2026-03-10 23:23:45 +0900

    chore: create router page

--- php_lesson/index.php
+++ php_lesson/index.php
@@ -1,13 +1,4 @@
 <?php
 
-require ("functions.php");
-
-$uri = parse_url($_SERVER["REQUEST_URI"])['path'];
-
-if ($uri === "/") {
-  require "controllers/index.php";
-} else if ($uri === "/about") {
-  require "controllers/about.php";
-} else if ($uri === "/contact") {
-  require "controllers/contact.php";
-}
+require("functions.php");
+require("router.php");
